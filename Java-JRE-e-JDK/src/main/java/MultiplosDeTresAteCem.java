186d945999705d06d862ded30505902f4407fe39
Wanessa Santos <wanessa.ufpe2018@gmail.com>
2022-09-05 17:02:28 -0300

    testando o \n

--- Java-JRE-e-JDK/src/main/java/MultiplosDeTresAteCem.java
+++ Java-JRE-e-JDK/src/main/java/MultiplosDeTresAteCem.java
@@ -1,9 +1,9 @@
 public class MultiplosDeTresAteCem {
     public static void main(String[] args) {
-        /*
-        * Utilize um laço do tipo for para imprimir todos os múltiplos de 3, entre 1 e 100.
-        *
-        * Dica: Existem duas maneiras tradicionais de resolver este problema. Uma delas consiste em fazer o for, e utilizar numero % 3 para descobrir o resto da divisão de um número por 3 (o operador % se chama de módulo). Se o resto for zero, ele é divisível por 3.
+        /* Utilize um laço do tipo for para imprimir todos os múltiplos de 3, entre 1 e 100.
+        * Dica: Existem duas maneiras tradicionais de resolver este problema. Uma delas consiste em
+        * fazer o for, e utilizar numero % 3 para descobrir o resto da divisão de um número por 3
+        * (o operador % se chama de módulo). Se o resto for zero, ele é divisível por 3.
         */
 
     }
